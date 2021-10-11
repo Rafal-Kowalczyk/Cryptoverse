67d537a0454f783504d063d6ee00353b201feaa8
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2021-10-11 14:44:17 +0200

    Added components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,59 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
+import {
+  Navbar,
+  Exchanges,
+  Cryptocurrencies,
+  CryptoDetalis,
+  Homepage,
+  News,
+} from "./components";
+import "./App.css";
+
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar">n</div>
-      <div className="main">m</div>
-      <div className="footer">f</div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+              <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetalis />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reservers
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
